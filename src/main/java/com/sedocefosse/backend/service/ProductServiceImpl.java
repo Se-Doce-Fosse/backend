8338e9f427cb6a92d4d8ac4fe30203afbc3716f9
Bruno <bruno8117@gmail.com>
2025-08-29 15:10:16 -0300

    feat: Adiciona endpoint get /products:/id para ver um produto

---
+++ src/main/java/com/sedocefosse/backend/service/ProductServiceImpl.java
@@ -0,0 +1,37 @@
+package com.sedocefosse.backend.service;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import com.sedocefosse.backend.model.Product;
+import com.sedocefosse.backend.repository.ProductRepository;
+
+@Service 
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Product create(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Optional<Product> findProductById(Long id) {
+        return productRepository.findById(id);
+    }
+}
+
+//Teste manual a ser feito no Postman
+//POST http://localhost:8080/api/products
+//Body (JSON):
+// {
+//     "name": "Produto Exemplo",
+//     "description": "Descrição do Produto Exemplo",
+//     "price": 19.99,
+//     "imageUrl": "http://exemplo.com/imagem.jpg",
+//     "restricaoAlimentar": "Sem glúten"
