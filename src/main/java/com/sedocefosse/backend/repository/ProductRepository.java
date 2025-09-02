d1ac836d224ccf88f986567d4c7d3f52702c76fc
Julian F. R. V. Silveira <julianvieirasilveira@gmail.com>
2025-09-02 20:14:10 -0300

    feat: configuração inicial do ProductRepository e ProductService

--- src/main/java/com/sedocefosse/backend/repository/ProductRepository.java
+++ src/main/java/com/sedocefosse/backend/repository/ProductRepository.java
@@ -1,5 +1,26 @@
 package com.sedocefosse.backend.repository;
 
-public class ProductRepository {
+import com.sedocefosse.backend.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
     
-}
+    Optional<Product> findBySku(String sku);
+    
+    boolean existsBySku(String sku);
+    
+    List<Product> findByAtivoTrue();
+    
+    List<Product> findByAtivoFalse();
+    
+    List<Product> findByDescricaoContainingIgnoreCase(String texto);
+    
