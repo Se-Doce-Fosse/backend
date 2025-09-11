03b88d0b74801f678fcdda8b2c6b51bb0e5928f5
Will0301 <willian.f.w@hotmail.com>
2025-09-11 10:48:12 -0300

    fix: limpeza de código

--- src/main/java/com/sedocefosse/backend/BackendApplication.java
+++ src/main/java/com/sedocefosse/backend/BackendApplication.java
@@ -9,21 +9,9 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.sql.DataSource;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.sedocefosse.backend.repository")
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
-    @Bean
-    public DataSource dataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://postgres:5432/db-se-doce-fosse");
-        dataSource.setUsername("admin");
-        dataSource.setPassword("admin");
-        return dataSource;
-    }
-
 }
