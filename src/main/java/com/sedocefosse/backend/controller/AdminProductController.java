dc12f98a1f55b6d8a039351fa818734464304d06
joaorhodenntc <jvrhodenmachado@hotmail.com>
2025-09-11 13:52:45 -0300

    fix: corrige controller de criação de produto e remove duplicidade

--- src/main/java/com/sedocefosse/backend/controller/AdminProductController.java
+++ src/main/java/com/sedocefosse/backend/controller/AdminProductController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +25,12 @@ public class AdminProductController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteProduct(@PathVariable String sku) {
         productService.deleteProductById(sku);
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        Product createdProduct = productService.create(product);
+        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
     }
   
 }
