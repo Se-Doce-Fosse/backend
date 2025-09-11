4e8eb79fd234e4b77254c0b21788f8490e4cf9ac
Davi Oliveira <davi.nunes@edu.pucrs.br>
2025-09-11 00:57:03 -0300

    Feat/122- Alter databases init SQL and adjust docker-compose

--- seed/postgresql-init.sql
+++ seed/postgresql-init.sql
@@ -90,17 +90,6 @@ CREATE TABLE "pedido_item" (
   PRIMARY KEY ("pedido_id", "produto_sku")
 );
 
-CREATE TABLE "user" (
-  "id" int PRIMARY KEY,
-  "cargo" varchar,
-  "nome" varchar,
-  "email" varchar UNIQUE,
-  "senha_hash" varchar,
-  "status" varchar,
-  "data_criacao" timestamp,
-  "ultimo_login" timestamp
-);
-
 CREATE TABLE "avaliacao" (
   "id" int PRIMARY KEY,
   "pedido_id" int,
@@ -136,36 +125,9 @@ ALTER TABLE "historico_compra" ADD FOREIGN KEY ("id_insumo") REFERENCES "estoque
 ALTER TABLE "historico_compra" ADD FOREIGN KEY ("id_unidade") REFERENCES "unidade" ("id");
 
 -- inserts iniciais
-INSERT INTO categoria (id, nome) VALUES (1, 'cookies');
-INSERT INTO categoria (id, nome) VALUES (2, 'bolos');
+INSERT INTO "categoria" VALUES (1, 'cookies');
+INSERT INTO "categoria" VALUES (2, 'bolos');
 
-INSERT INTO "produto" VALUES ('SKU001', 'COOKIE CHOCOLATE', 'COOKIE DE CHOCOLATE BELGA PREMIUM', 35.9, '-', True);
-INSERT INTO "produto" VALUES ('SKU002', 'COOKIE AVELA', 'COOKIE DE AVELA COM PEDACOS CROCANTES', 38.5, '-', False);
-INSERT INTO "produto" VALUES ('SKU003', 'COOKIE RED VELVET', 'COOKIE SABOR RED VELVET CREMOSO', 42.0, '-', True);
-INSERT INTO "produto" VALUES ('SKU004', 'COOKIE OREO', 'COOKIE COM PEDACOS DE BISCOITO OREO', 37.9, '-', False);
-INSERT INTO "produto" VALUES ('SKU005', 'COOKIE NUTELLA', 'COOKIE RECHEADO COM NUTELLA', 45.0, '-', True);
-INSERT INTO "produto" VALUES ('SKU006', 'COOKIE LEITE NINHO', 'COOKIE SABOR LEITE EM PO NINHO', 36.5, '-', True);
-INSERT INTO "produto" VALUES ('SKU007', 'COOKIE AMENDOIM', 'COOKIE DE AMENDOIM COM MEL', 32.9, '-', False);
-INSERT INTO "produto" VALUES ('SKU008', 'COOKIE BAUNILHA', 'COOKIE CLASSICO DE BAUNILHA', 30.0, '-', True);
-INSERT INTO "produto" VALUES ('SKU009', 'COOKIE CANELA', 'COOKIE AROMATIZADO COM CANELA', 33.5, '-', False);
-INSERT INTO "produto" VALUES ('SKU010', 'COOKIE MMS', 'COOKIE COLORIDO COM MMS', 39.9, '-', True);
-INSERT INTO "produto" VALUES ('SKU011', 'COOKIE LIMÃO', 'COOKIE CÍTRICO SABOR LIMÃO SICILIANO', 34.0, '-', True);
-INSERT INTO "produto" VALUES ('SKU012', 'COOKIE COCO', 'COOKIE DE COCO QUEIMADO', 31.9, '-', False);
-INSERT INTO "produto" VALUES ('SKU013', 'COOKIE CAFÉ', 'COOKIE SABOR CAFÉ EXPRESSO', 36.0, '-', True);
-INSERT INTO "produto" VALUES ('SKU014', 'COOKIE BRIGADEIRO', 'COOKIE SABOR BRIGADEIRO GOURMET', 40.5, '-', False);
-INSERT INTO "produto" VALUES ('SKU015', 'COOKIE AMERICANO', 'COOKIE ESTILO AMERICANO TRADICIONAL', 35.0, '-', True);
-INSERT INTO "produto" VALUES ('SKU016', 'COOKIE CASTANHA', 'COOKIE DE CASTANHA-DO-PARÁ', 43.9, '-', True);
-INSERT INTO "produto" VALUES ('SKU017', 'COOKIE MARACUJÁ', 'COOKIE COM RECHEIO DE MARACUJÁ', 37.0, '-', False);
-INSERT INTO "produto" VALUES ('SKU018', 'COOKIE LEITE CONDENSADO', 'COOKIE SABOR LEITE CONDENSADO CASEIRO', 38.0, '-', True);
-INSERT INTO "produto" VALUES ('SKU019', 'COOKIE AMENDOAS', 'COOKIE COM AMENDOAS LAMINADAS', 41.5, '-', True);
-INSERT INTO "produto" VALUES ('SKU020', 'COOKIE MORANGO', 'COOKIE SABOR MORANGO COM CHOCOLATE', 36.9, '-', False);
-INSERT INTO "produto" VALUES ('SKU021', 'COOKIE NOZES', 'COOKIE COM NOZES PICADAS', 39.5, '-', True);
-INSERT INTO "produto" VALUES ('SKU022', 'COOKIE BRANCO', 'COOKIE DE CHOCOLATE BRANCO', 35.9, '-', False);
-INSERT INTO "produto" VALUES ('SKU023', 'COOKIE AVELA CROCANTE', 'COOKIE COM AVELA E TEXTURA CROCANTE', 40.0, '-', True);
-INSERT INTO "produto" VALUES ('SKU024', 'COOKIE MAÇA CANELA', 'COOKIE SABOR MAÇÃ COM CANELA', 34.5, '-', True);
-INSERT INTO "produto" VALUES ('SKU025', 'COOKIE DARK CHOCOLATE', 'COOKIE DE CHOCOLATE 70% CACAU', 42.5, '-', False);
-INSERT INTO "produto" VALUES ('SKU026', 'COOKIE AMENDOIM CARAMELO', 'COOKIE DE AMENDOIM COM CARAMELO', 37.5, '-', True);
-INSERT INTO "produto" VALUES ('SKU027', 'COOKIE BANANA', 'COOKIE SABOR BANANA COM CANELA', 33.0, '-', True);
-INSERT INTO "produto" VALUES ('SKU028', 'COOKIE LARANJA', 'COOKIE CÍTRICO SABOR LARANJA', 32.5, '-', False);
-INSERT INTO "produto" VALUES ('SKU029', 'COOKIE MENTA CHOCOLATE', 'COOKIE SABOR MENTA COM CHOCOLATE', 39.0, '-', True);
-INSERT INTO "produto" VALUES ('SKU030', 'COOKIE TRIPLO CHOCOLATE', 'COOKIE COM TRÊS TIPOS DE CHOCOLATE', 44.9, '-', True);
+INSERT INTO "produto" VALUES ('SKU001', 'Cookie Oreo com Nutella', 'Delicioso cookie com recheio de Nutella e pedaços de Oreo.', 35.9, '/images/cookie-oreo.jpg', True, 1);
+INSERT INTO "produto" VALUES ('SKU002', 'Cookie Chocolate Branco', 'Cookie macio com gotas de chocolate branco.', 38.5, '/images/cookie-branco.jpg', False, 1);
+INSERT INTO "produto" VALUES ('SKU003', 'Bolo Red Velvet', 'Clássico bolo Red Velvet com cobertura de cream cheese.', 42.0, '/images/bolo-red-velvet.jpg', True, 2);
