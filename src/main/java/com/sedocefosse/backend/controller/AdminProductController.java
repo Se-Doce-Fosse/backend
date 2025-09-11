ce95c3e86a0da663eaee56dd787f3c88ba998587
Isabella <isabellalbecker@gmail.com>
2025-09-11 20:54:06 -0300

    retorno do create e gerar sku aleatório

--- src/main/java/com/sedocefosse/backend/controller/AdminProductController.java
+++ src/main/java/com/sedocefosse/backend/controller/AdminProductController.java
@@ -1,12 +1,12 @@
 package com.sedocefosse.backend.controller;
 
+import com.sedocefosse.backend.dto.ProductDetailsDTO;
 import com.sedocefosse.backend.model.Product;
 import com.sedocefosse.backend.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,11 +29,11 @@ public class AdminProductController {
     }
 
     @PostMapping
-    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
-        Product createdProduct = productService.create(product);
+    public ResponseEntity<ProductDetailsDTO> createProduct(@RequestBody Product product) {
+        ProductDetailsDTO createdProduct = productService.create(product);
         return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
     }
-
+  
     @PatchMapping("/{id}/status")
     public ResponseEntity<Product> toggleProductStatus(@PathVariable String id) {
         Product updatedProduct = productService.toggleStatus(id);
@@ -42,5 +42,4 @@ public class AdminProductController {
         }
         return ResponseEntity.ok(updatedProduct);
     }
-  
 }
--- src/main/java/com/sedocefosse/backend/service/ProductServiceImpl.java
+++ src/main/java/com/sedocefosse/backend/service/ProductServiceImpl.java
@@ -2,6 +2,7 @@ package com.sedocefosse.backend.service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
@@ -26,8 +27,10 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    public Product create(Product product) {
-        return productRepository.save(product);
+    public ProductDetailsDTO create(Product product) {        
+        product.setSku("SKU-" + UUID.randomUUID().toString().substring(0, 8).toUpperCase());        
+        Product savedProduct = productRepository.save(product);
+        return mapToProductDetailsDTO(savedProduct);
     }
 
     @Override
