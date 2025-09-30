218f4380bf4b771f73637dfa9b65ff6ecf6efb8b
gsantosdasilva <contatogsantosdasilva@gmail.com>
2025-09-29 21:50:47 -0300

    fix: removendo do endpoint publico e adicionando no endpoint admin

--- src/main/java/com/sedocefosse/backend/controller/ProductController.java
+++ src/main/java/com/sedocefosse/backend/controller/ProductController.java
@@ -14,8 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,12 +42,5 @@ public class ProductController {
 
         return productDto.map(ResponseEntity::ok)
                         .orElseGet(() -> ResponseEntity.notFound().build());
-    }
-    
-    //http://localhost:8081/products/{sku}
-    @PutMapping("/{sku}")
-    public ResponseEntity<Product> updateProduct(@PathVariable String sku, @RequestBody Product productDetails) {
-        Product updatedProduct = productService.updateProduct(sku, productDetails);
-        return ResponseEntity.ok(updatedProduct);
     }
 }
