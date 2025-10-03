2dcf4d0d8ec8ede39fa1c1a52858ad5c7752aa63
Isabella Becker <isabellalbecker@gmail.com>
2025-10-03 00:12:21 -0300

    fix cors

--- src/main/java/com/sedocefosse/backend/configs/cors/CorsConfig.java
+++ src/main/java/com/sedocefosse/backend/configs/cors/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
       .allowedOrigins("http://localhost:5173", "http://localhost:3000")
-      .allowedMethods("","POST","PGETUT","PATCH","DELETE","OPTIONS")
+      .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
       .allowedHeaders("*")
       .allowCredentials(true);
   }
