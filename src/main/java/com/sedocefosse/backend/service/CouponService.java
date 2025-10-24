34e8b226e48c74a45a99ba60cbcd71eba11720d2
Davi Oliveira <davi.nunes@edu.pucrs.br>
2025-10-24 00:31:50 -0300

    feat: adiciona endpoint para apagar e atualizar um cupom

--- src/main/java/com/sedocefosse/backend/service/CouponService.java
+++ src/main/java/com/sedocefosse/backend/service/CouponService.java
@@ -1,9 +1,17 @@
 package com.sedocefosse.backend.service;
 
+import com.sedocefosse.backend.dto.CouponDTO;
+import com.sedocefosse.backend.dto.SupplyResponseDTO;
+import com.sedocefosse.backend.dto.SupplyUpdateDTO;
 import com.sedocefosse.backend.model.Coupon;
 import java.util.List;
 
 public interface CouponService {
     
     List<Coupon> findAll();
+
+    void deleteCuponById(Long cuponId);
+
+    CouponDTO update(Long id, CouponDTO updateDTO);
+
 }
--- src/main/java/com/sedocefosse/backend/service/CouponServiceImpl.java
+++ src/main/java/com/sedocefosse/backend/service/CouponServiceImpl.java
@@ -1,5 +1,7 @@
 package com.sedocefosse.backend.service;
 
+import com.sedocefosse.backend.configs.exceptions.ResourceNotFoundException;
+import com.sedocefosse.backend.dto.CouponDTO;
 import org.springframework.stereotype.Service;
 
 import com.sedocefosse.backend.model.Coupon;
@@ -11,7 +13,7 @@ import java.util.List;
 public class CouponServiceImpl implements CouponService {
     
     private final CouponRepository couponRepository;
-    
+
     public CouponServiceImpl(CouponRepository couponRepository) {
         this.couponRepository = couponRepository;
     }
@@ -20,4 +22,44 @@ public class CouponServiceImpl implements CouponService {
     public List<Coupon> findAll() {
         return couponRepository.findAll();
     }
+
+    @Override
+    public void deleteCuponById(Long cuponId) {
+        if (!couponRepository.existsById(cuponId)) {
+            throw new ResourceNotFoundException("Cupom não encontrado com o ID: "+cuponId);
+        }
+
+        couponRepository.deleteById(cuponId);
+    }
+
+    @Override
+    public CouponDTO update(Long id, CouponDTO updateDTO) {
+        Coupon couponExisted = couponRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Cupom não encontrado com o ID: "+id));
+
+        couponExisted.setAtivo(updateDTO.getAtivo());
+        couponExisted.setCodigo(updateDTO.getCodigo());
+        couponExisted.setValidade(updateDTO.getValidade());
+        couponExisted.setUnico(updateDTO.getUnico());
+        couponExisted.setValorDesc(updateDTO.getValorDesc());
+
+        Coupon couponUpdated = couponRepository.save(couponExisted);
+
+        return toDTO(couponUpdated);
+    }
+
+    public CouponDTO toDTO(Coupon coupon) {
+        CouponDTO couponDTO = new CouponDTO();
+
+        couponDTO.setId(coupon.getId());
+        couponDTO.setAtivo(coupon.getAtivo());
+        couponDTO.setCodigo(coupon.getCodigo());
+        couponDTO.setValidade(coupon.getValidade());
+        couponDTO.setUnico(coupon.getUnico());
+        couponDTO.setValorDesc(coupon.getValorDesc());
+
+        return couponDTO;
+    }
+
+
 }
