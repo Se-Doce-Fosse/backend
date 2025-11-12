f8309b7f45d4b28648dba04e4113011216428332
Isabella Becker <isabellalbecker@gmail.com>
2025-11-11 22:26:37 -0300

    fix: correção do banco para criar pedido

--- seed/postgresql-init.sql
+++ seed/postgresql-init.sql
@@ -8,7 +8,6 @@ DROP TABLE IF EXISTS "produto_ingrediente" CASCADE;
 DROP TABLE IF EXISTS "produto" CASCADE;
 DROP TABLE IF EXISTS "estoque_insumos" CASCADE;
 DROP TABLE IF EXISTS "unidade" CASCADE;
-DROP TABLE IF EXISTS "user" CASCADE;
 drop table if exists "admin" cascade;
 
 CREATE TABLE "unidade" (
@@ -33,7 +32,8 @@ CREATE TABLE "produto" (
 "descricao" varchar,
 "valor" numeric,
 "imagem_url" varchar,
-"ativo" boolean
+"ativo" boolean,
+"quantidade" int
 );
 
 CREATE TABLE "produto_ingrediente" (
@@ -56,12 +56,6 @@ CREATE TABLE "cupom_produto" (
 "cupom_id" int,
 "produto_sku" varchar,
 PRIMARY KEY ("cupom_id", "produto_sku")
-);
-
-CREATE TABLE "user" (
-"id" varchar PRIMARY KEY,
-"nome" varchar,
-"telefone" varchar
 );
 
 CREATE TABLE "pedido" (
@@ -111,12 +105,10 @@ ALTER TABLE "produto_ingrediente" ADD FOREIGN KEY ("produto_sku") REFERENCES "pr
 ALTER TABLE "produto_ingrediente" ADD FOREIGN KEY ("ingrediente_id") REFERENCES "estoque_insumos" ("id");
 ALTER TABLE "cupom_produto" ADD FOREIGN KEY ("cupom_id") REFERENCES "cupom" ("id");
 ALTER TABLE "cupom_produto" ADD FOREIGN KEY ("produto_sku") REFERENCES "produto" ("sku");
-ALTER TABLE "pedido" ADD FOREIGN KEY ("cliente_id") REFERENCES "user" ("id");
 ALTER TABLE "pedido" ADD FOREIGN KEY ("cupom_id") REFERENCES "cupom" ("id");
 ALTER TABLE "pedido_item" ADD FOREIGN KEY ("pedido_id") REFERENCES "pedido" ("id");
 ALTER TABLE "pedido_item" ADD FOREIGN KEY ("produto_sku") REFERENCES "produto" ("sku");
 ALTER TABLE "avaliacao" ADD FOREIGN KEY ("pedido_id") REFERENCES "pedido" ("id");
-ALTER TABLE "avaliacao" ADD FOREIGN KEY ("cliente_id") REFERENCES "user" ("id");
 ALTER TABLE "historico_compra" ADD FOREIGN KEY ("id_insumo") REFERENCES "estoque_insumos" ("id");
 ALTER TABLE "historico_compra" ADD FOREIGN KEY ("id_unidade") REFERENCES "unidade" ("id");
 
@@ -125,11 +117,6 @@ INSERT INTO "unidade" ("nome", "base_preco_compra") VALUES
 ('KG', 1000.00),
 ('UN', 1.00),
 ('L', 1.00);
-
-INSERT INTO "user" ("id","nome", "telefone") VALUES
-('1','Alice Smith', '987654321'),
-('2','Bob Johnson', '555123456'),
-('3','Carlos Oliveira', '11998765432');
 
 INSERT INTO "admin" ("username", "email", "password", "role") VALUES
 ('admin_master', 'admin@email.com', '$2a$10$CKonyZomgg/CVkYBy.Ex9.stAr8SBerpSE8igTBlk5I..YRniz4ta', 'ROLE_OWNER');
@@ -153,15 +140,15 @@ INSERT INTO "estoque_insumos" ("nome", "id_unidade", "quantidade", "preco_compra
 ('Embalagem Individual para Cookie', 2, 500, 0.50, 100, true),
 ('Caixa para Bolo Pequeno', 2, 50, 3.00, 10, true);
 
-INSERT INTO "produto" ("sku", "nome", "descricao", "valor", "imagem_url", "ativo") VALUES
-('CK001', 'Cookie Clássico com Gotas de Chocolate', 'Massa amanteigada com baunilha e gotas de chocolate meio amargo.', 41.50, 'https://static.ifood-static.com.br/image/upload/t_medium/pratos/96aa4ce9-22d9-4993-8f81-01caf03c2d31/202506161754_8J5A_i.jpg', true),
-('CK002', 'Cookie Chocolate Branco', 'Prepare-se para uma verdadeira explosão de sabores com o nosso irresistível Cookie de chocolate branco! ', 15.50, 'https://static.ifood-static.com.br/image/upload/t_medium/pratos/96aa4ce9-22d9-4993-8f81-01caf03c2d31/202502272218_TE6C_i.jpg', true),
-('CK003', 'Cookie Dark Chocolate', 'Uma verdadeira explosão de sabores para os chocólatras de plantão! Nossa massa rica e intensa em cacau é a base perfeita para pedaços generosos de chocolate branco, ao leite e meio amargo, que se derretem delicadamente em cada mordida.', 52.50, 'https://static.ifood-static.com.br/image/upload/t_medium/pratos/96aa4ce9-22d9-4993-8f81-01caf03c2d31/202506161756_NB65_i.jpg', true),
-('CK004', 'Cookie Nozes com Doce de Leite', 'Mergulhe em um mundo de sabor com o nosso Cookie de Nozes com Doce de Leite, uma verdadeira obra-prima que vai encantar seus sentidos! ', 17.90, 'https://static.ifood-static.com.br/image/upload/t_medium/pratos/96aa4ce9-22d9-4993-8f81-01caf03c2d31/202506161909_5T2Y_i.jpg', true),
-('CK005', 'Cookie Romeu e Julieta', 'Uma releitura irresistível do clássico brasileiro!', 15.90, 'https://static.ifood-static.com.br/image/upload/t_medium/pratos/96aa4ce9-22d9-4993-8f81-01caf03c2d31/202506162235_DC6F_i.jpg', true),
-('BL001', 'Brownie com Nutella', 'Marmitinha de brownie com um delicioso recheio de Nutella', 20.90, 'https://static.ifood-static.com.br/image/upload/t_medium/pratos/96aa4ce9-22d9-4993-8f81-01caf03c2d31/202502272219_4KDY_i.jpg', true),
-('BL002', 'Bolo de Chocolate Intenso (fatia)', 'Fatia generosa de bolo de chocolate com cobertura de brigadeiro.', 35.00, 'https://static.ifood-static.com.br/image/upload/t_medium/pratos/96aa4ce9-22d9-4993-8f81-01caf03c2d31/202508091609_49LG_.jpeg', true),
-('BL003', 'Bolo de Cenoura com Chocolate (inteiro)', 'Bolo fofinho de cenoura com cobertura de chocolate. Serve 8 pessoas.', 25.00, 'https://static.ifood-static.com.br/image/upload/t_medium/pratos/96aa4ce9-22d9-4993-8f81-01caf03c2d31/202508091609_49LG_.jpeg', true);
+INSERT INTO "produto" ("sku", "nome", "descricao", "valor", "imagem_url", "ativo", "quantidade") VALUES
+('CK001', 'Cookie Clássico com Gotas de Chocolate', 'Massa amanteigada com baunilha e gotas de chocolate meio amargo.', 41.50, 'https://static.ifood-static.com.br/image/upload/t_medium/pratos/96aa4ce9-22d9-4993-8f81-01caf03c2d31/202506161754_8J5A_i.jpg', true, 10),
+('CK002', 'Cookie Chocolate Branco', 'Prepare-se para uma verdadeira explosão de sabores com o nosso irresistível Cookie de chocolate branco! ', 15.50, 'https://static.ifood-static.com.br/image/upload/t_medium/pratos/96aa4ce9-22d9-4993-8f81-01caf03c2d31/202502272218_TE6C_i.jpg', true, 10),
+('CK003', 'Cookie Dark Chocolate', 'Uma verdadeira explosão de sabores para os chocólatras de plantão! Nossa massa rica e intensa em cacau é a base perfeita para pedaços generosos de chocolate branco, ao leite e meio amargo, que se derretem delicadamente em cada mordida.', 52.50, 'https://static.ifood-static.com.br/image/upload/t_medium/pratos/96aa4ce9-22d9-4993-8f81-01caf03c2d31/202506161756_NB65_i.jpg', true, 10),
+('CK004', 'Cookie Nozes com Doce de Leite', 'Mergulhe em um mundo de sabor com o nosso Cookie de Nozes com Doce de Leite, uma verdadeira obra-prima que vai encantar seus sentidos! ', 17.90, 'https://static.ifood-static.com.br/image/upload/t_medium/pratos/96aa4ce9-22d9-4993-8f81-01caf03c2d31/202506161909_5T2Y_i.jpg', true, 10),
+('CK005', 'Cookie Romeu e Julieta', 'Uma releitura irresistível do clássico brasileiro!', 15.90, 'https://static.ifood-static.com.br/image/upload/t_medium/pratos/96aa4ce9-22d9-4993-8f81-01caf03c2d31/202506162235_DC6F_i.jpg', true, 10),
+('BL001', 'Brownie com Nutella', 'Marmitinha de brownie com um delicioso recheio de Nutella', 20.90, 'https://static.ifood-static.com.br/image/upload/t_medium/pratos/96aa4ce9-22d9-4993-8f81-01caf03c2d31/202502272219_4KDY_i.jpg', true, 10),
+('BL002', 'Bolo de Chocolate Intenso (fatia)', 'Fatia generosa de bolo de chocolate com cobertura de brigadeiro.', 35.00, 'https://static.ifood-static.com.br/image/upload/t_medium/pratos/96aa4ce9-22d9-4993-8f81-01caf03c2d31/202508091609_49LG_.jpeg', true, 10),
+('BL003', 'Bolo de Cenoura com Chocolate (inteiro)', 'Bolo fofinho de cenoura com cobertura de chocolate. Serve 8 pessoas.', 25.00, 'https://static.ifood-static.com.br/image/upload/t_medium/pratos/96aa4ce9-22d9-4993-8f81-01caf03c2d31/202508091609_49LG_.jpeg', true, 10);
 
 INSERT INTO "produto_ingrediente" ("produto_sku", "ingrediente_id", "quantidade_utilizada") VALUES
 ('CK001', 1, 0.5),
@@ -181,11 +168,6 @@ INSERT INTO "cupom_produto" ("cupom_id", "produto_sku") VALUES
 INSERT INTO "historico_compra" ("id_insumo", "id_unidade", "quantidade", "peco_unidade") VALUES
 (1, 1, 25, 4.20),
 (5, 1, 5, 42.50);
-
-INSERT INTO "pedido" ("cliente_id", "data_criacao", "valor_total", "status", "cupom_id") VALUES
-('1', '2025-09-26 14:00:00', 13.00, 'ENTREGUE', 2),
-('1', '2025-09-27 18:10:00', 45.00, 'PAGAMENTO_APROVADO', 3),
-('2', NOW(), 22.00, 'EM_PREPARACAO', NULL);
 
 INSERT INTO "pedido_item" ("pedido_id", "produto_sku", "quantidade", "valor_unitario") VALUES
 (1, 'CK001', 4, 4.50),
