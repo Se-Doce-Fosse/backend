b1340b804c6a6e718cb63c7b49693dbb33cecbe2
joaorhodenntc <jvrhodenmachado@hotmail.com>
2025-11-14 18:33:45 -0300

    allowed ip

--- src/main/java/com/sedocefosse/backend/configs/cors/CorsConfig.java
+++ src/main/java/com/sedocefosse/backend/configs/cors/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-      .allowedOrigins("http://localhost:5173", "http://localhost:3000")
+      .allowedOrigins("http://localhost:5173", "http://localhost:3000", "http://3.130.152.221/")
       .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
       .allowedHeaders("*")
       .allowCredentials(true);
